ff5aab855f172e09c06c5e94c602e4c4f91eece2
AlexGebrah <alexisgebrah@gmail.com>
2025-07-31 13:01:54 +0300

    first commit

--- src/main/java/telran/java58/person/service/PersonServiceImpl.java
+++ src/main/java/telran/java58/person/service/PersonServiceImpl.java
@@ -55,6 +55,12 @@ public class PersonServiceImpl implements PersonService, CommandLineRunner {
     @Transactional
     public PersonDto deletePerson(int id) {
         Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+        if(person instanceof Child){
+            return modelMapper.map(person, ChildDto.class);
+        }
+        if(person instanceof Employee){
+            return modelMapper.map(person, EmployeeDto.class);
+        }
         personRepository.delete(person);
         return modelMapper.map(person, PersonDto.class);
     }
